f50ff296d8c8d8b35c491b353d62864024810784
Kristian Beall <beallkristian@gmail.com>
2022-08-30 19:11:36 -0400

    created IUserService and UserService folder and files

---
+++ Flippd.Services/User/UserService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flippd.Services.User
+{
+    public class UserService : IUserService
+    {
+        
+    }
+}
