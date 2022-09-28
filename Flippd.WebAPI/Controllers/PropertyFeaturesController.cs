1149d1763c67994aceb0a8fe493bd7e7c9f34409
Connor Fowler <connorfowlerroth@gmail.com>
2022-09-28 17:44:14 -0400

    Finished GetPropertyFeaturesByPropertyFeaturesIdAsync, UpdatePropertyFeaturesAsync, and DeletePropertyFeaturesAsync

--- Flippd.WebAPI/Controllers/PropertyFeaturesController.cs
+++ Flippd.WebAPI/Controllers/PropertyFeaturesController.cs
@@ -39,14 +39,14 @@ namespace Flippd.WebAPI.Controllers
         [HttpGet("{listingId:int}")]
         public async Task<IActionResult> GetById([FromRoute] int listingId)
         {
-            var PropertyFeaturesDetail = await _service.GetPropertyFeaturesByListingIdAsync(listingId);
+            var propertyFeaturesDetail = await _service.GetPropertyFeaturesByPropertyFeaturesIdAsync(listingId);
 
-            if (PropertyFeaturesDetail is null)
+            if (propertyFeaturesDetail is null)
             {
                 return NotFound();
             }
 
-            return Ok(PropertyFeaturesDetail);
+            return Ok(propertyFeaturesDetail);
         }
 
         // PUT 
@@ -59,6 +59,15 @@ namespace Flippd.WebAPI.Controllers
             return await _service.UpdatePropertyFeaturesAsync(request)
             ? Ok("Property Features updated successfully.")
             : BadRequest("Property Features could not be updated.");
+        }
+
+        // DELETE 
+        [HttpDelete("{PropertyFeaturesId:int}")]
+        public async Task<IActionResult> DeletePropertyFeatures([FromRoute] int propertyFeaturesId)
+        {
+            return await _service.DeletePropertyFeaturesAsync(propertyFeaturesId)
+            ? Ok($"Property {propertyFeaturesId} features successfully deleted.")
+            : BadRequest($"Property {propertyFeaturesId} features could not be deleted.");
         }
     }
 }
